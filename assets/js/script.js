0b17f5551de2c9761def7d6f597b377c33d2cdf2
Fenrir117 <fenrir.117@outlook.fr>
2019-04-24 09:49:06 +0000

    Javascript ok, plus que les détails pour terminer

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,112 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// Formulaire
+
+errorFirstName = document.querySelector('#erreurfirstname');
+firstNameInput = document.querySelector('#firstname');
+errorMessage = document.querySelector('#erreurmessage');
+messageInput = document.querySelector('#message');
+errorMail = document.querySelector('#erreurmail');
+mailInput = document.querySelector('#mail');
+errorValidation = document.querySelector('#erreurvalidation');
+validationInput = document.querySelector('#submit');
+
+
+firstNameInput.addEventListener('blur', erreurFirstName);
+messageInput.addEventListener('blur', erreurMessage);
+mailInput.addEventListener('blur', erreurMail);
+document.querySelector('#submit').addEventListener('click', validation);
+
+
+validationFirstName = false;
+validationMessage = false;
+validationMail = false;
+
+function erreurFirstName() {
+
+	var letters = /^[A-Za-z-é'èàçêâîôëäïö]+$/;
+	
+
+	if (document.querySelector('#firstname').value.match(letters)) {
+		errorFirstName.innerHTML = "";
+		validationFirstName = true;
+
+	} else if (document.querySelector('#firstname').value === ("")) {
+		errorFirstName.innerHTML = "Veuillez renseigner votre prénom";
+		console.log("aa");
+		validationFirstName = false;
+
+	} else {
+
+		errorFirstName.innerHTML = "Veuillez utiliser des caractere.";
+		validationFirstName = false;
+	}
+}
+
+
+function erreurMessage() {
+
+	var letters = /^[A-Z0-9a-z-é'èàçêâîôëäïö,0-9]+$/;
+	if (document.querySelector('#message').value.match(letters)) {
+
+		errorMessage.innerHTML = "";
+		validationMessage = true;
+
+	} else if (document.querySelector('#message').value == ("")) {	
+    errorMessage.innerHTML = "Veuillez renseigner votre Message";
+    console.log("yep");
+		validationMessage = false;
+
+	} else {	
+    errorMessage.innerHTML = "Veuillez utiliser les caractères autorisés";
+    console.log("test");
+		validationMessage = false;
+	}
+}
+
+
+function erreurMail () {
+
+  var letters = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var lettersValide = /\S+@\S+\.\S+/;
+
+ 
+	if (document.querySelector('#mail').value.match(letters) && document.querySelector('#mail').value.match(lettersValide)) {
+
+		errorMail.innerHTML = "";
+		validationMail = true;
+
+	} else if (document.querySelector('#mail').value == ("")) {	
+    errorMail.innerHTML = "Veuillez renseigner votre E-mail";
+    console.log("yep");
+		validationMail = false;
+
+	} else {	
+    errorMail.innerHTML = "Veuillez utiliser les caractères autorisés";
+    console.log("test");
+		validationMail = false;
+	}
+
+}
+
+
+function validation(e) {
+	console.log("gg");
+
+	if(validationFirstName === false || validationMessage === false || validationMail === false){
+
+			errorValidation.innerHTML = "Un champs est manquant ou invalide";
+			e.preventDefault();
+			return false;
+	} 
+
+	
+	 else {
+			alert("Formulaire envoyé !");
+
+
+			formulaire.submit();
+	}	 
+}; 
